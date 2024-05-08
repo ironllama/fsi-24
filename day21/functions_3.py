3310f7ae65a4d28dd6580fad1b60fd96aa6ef7ca
Alex <alexoh@wcoding.com>
2024-05-08 17:26:39 +0900

    day21

---
+++ day21/functions_3.py
@@ -0,0 +1,35 @@
+# 1. Write a function to find the largest of three numbers, without using
+#     the built-in `max` function.
+
+
+# 2. Write a function to sum all the numbers in a list, without using the
+#     built-in `sum` function.
+
+
+# 3. Write a function to multiply all the numbers in a list, without using
+#     any library functions.
+
+
+# 4. Write a function to reverse a string, without using the built-in
+#     `reverse` function.
+
+
+# 5. Write a function called 'withinRange' that takes three parameters,
+#     a number you are checking for, the first number to check against and
+#     the last number to check against. Check whether the first number falls
+#     in the other numbers (inclusive) and return a boolean.
+
+
+# 6. Write a function that takes a list and returns a new list with
+#     unique elements of the first list.
+
+
+# 7. Write a function that takes a number as a parameter and checks if the
+#     number is prime or not.
+
+
+# 8. Write a function to print only the even numbers from a given list.
+
+
+# 9. Write a function to create and print the list of squares for the
+#      numbers between 1 and 30 (both included).
