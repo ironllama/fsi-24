6f8b57d957e5658400d5d1a2f63a7b9e8b3fe3ea
Alex <alexoh@wcoding.com>
2024-05-08 17:25:13 +0900

    day21

---
+++ day21/functions_2.py
@@ -0,0 +1,32 @@
+# 1. Write a Python function that takes a list of words and returns the
+#     longest word and the length of the longest word.
+
+#    b. Call the function with positional arguments
+
+#    c. Call the function with keyword arguments
+
+
+# 2. Write a Python function to remove the n-th index character from a
+#     nonempty string.
+
+#    b. Call the function with positional arguments
+
+#    c. Call the function with keyword arguments
+
+
+# 3. Create a function called add_title which takes a name and gender as
+#     arguments and returns either "Mr. <name>" or "Ms. <name>"
+#     (eg. Calling `add_title("Pam", "F")` returns "Ms. Pam")
+
+#    b. Call the function with positional arguments
+
+#    c. Call the function with keyword arguments
+
+
+# 4. Create a function called multiply_elements which takes a list and a
+#     number as arguments. It multiplies each element in the list by the
+#     number, and returns the list
+
+#    b. Call the function with positional arguments
+
+#    c. Call the function with keyword arguments
